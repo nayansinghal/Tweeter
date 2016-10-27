09de620ee4f896d158f04b05146b34ec8cb85fc3
nayansinghal <singhalnayan91@gmail.com>
2016-10-27 17:48:43 -0500

    saving files to output folder

--- visualize.py
+++ visualize.py
@@ -2,9 +2,9 @@ from gensim import corpora, models
 import pyLDAvis.gensim
 import matplotlib.pyplot as plt
 
-corpus = corpora.MmCorpus('data/corpus.mm')
-dictionary = corpora.Dictionary.load('data/dictionary.dict')
+corpus = corpora.MmCorpus('output/corpus.mm')
+dictionary = corpora.Dictionary.load('output/dictionary.dict')
 
-lda = models.LdaModel.load('data/document.lda')
+lda = models.LdaModel.load('output/document.lda')
 followers_data =  pyLDAvis.gensim.prepare(lda, corpus, dictionary)
-pyLDAvis.save_html(followers_data, 'hello.html')
+pyLDAvis.save_html(followers_data, 'output/LdaModel.html')
